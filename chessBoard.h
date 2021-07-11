a090a117519f15311d55cc9f13b8db6e148d7c73
alizeinodin <alizeinodin79@gmail.com>
2021-07-11 11:57:00 +0430

    edit chessBoard.h

--- chessBoard.h
+++ chessBoard.h
@@ -8,17 +8,11 @@ class ChessBoard
     private:
     ChessBoard(); // singleton pattern
     std::array<std::array<Check, 8>, 8> Board;
-    PositiveScore pscore; 
-    NegativeScore nscore;
-
 
     public:
     static ChessBoard & getInstance(); // sinleton pattern
     std::array<std::array<Check, 8>, 8> start();
     std::array<std::array<Check, 8>, 8> remmeber();
-    void addScore(size_t state, int score); // state 1 for psocre, 2 for nscore
-
     
-}
-
+};
 #endif /* _CHESSBOARD_H*/
