6aff70b17b7746225051f1bec2d3a7271876d02d
erfan-hamidi <83178650+erfan-hamidi@users.noreply.github.com>
2021-07-15 00:47:55 +0430

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "king.h"
 #include "util.h"
 #include "pawn.h"
@@ -11,46 +10,6 @@ int main(int argc, char const *argv[])
 {
     rook p("#000000");
     p.access("B2");
-// =======
-// #include "id.h"
-// #include <array>
-// #include "cell.h"
-// #include "chessBoard.h"
-// using namespace std;
-
-// int main()
-// {
-//     // ChessBoard myBoard = ChessBoard::getInstance();
-//     // myBoard.testColor();
-//     // array<array<Cell, 8>, 8> test;
-
-//     // for (size_t i = 0; i < 8; i++)
-//     // {
-//     //     for (size_t k = 0; k < 8; k++)
-//     //     {
-//     //         test[i][k] = CellID();
-//     //         // cout << CellID() << "\t";
-//     //     }
-//     //     // cout << endl;
-//     // }
-//     // for (size_t i = 0; i < 8; i++)
-//     // {
-//     //     for (size_t k = 0; k < 8; k++)
-//     //     {
-//     //         cout << test[i][k].getColor() << "\t";
-//     //     }
-//     //     cout << endl;
-//     // }
-// >>>>>>> 534eac2a9fe7241af6bf8629b7c368f4b8fd42a4
-//     return 0;
-// }
-// =======
-// #include "king.h"
-// #include "util.h"
-// int main(int argc, char const *argv[])
-// {
-//     king k("wi");
-//     k.movePiece("KE1F1");
-//     return 0;
-// }
-// >>>>>>> 233d1494294eac9c8bf65f7e6de05ab5f6cf6c16
+    
+    return 0;
+}
