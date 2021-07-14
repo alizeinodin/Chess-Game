bc01c92cc27c714b20c675eb2d1bcdf3ceba1f9c
erfan-hamidi <hamidierfan716@gmail.com>
2021-07-14 23:56:57 +0430

    developing rook class

--- include/chessMan.h
+++ include/chessMan.h
@@ -18,7 +18,7 @@ public:
     virtual bool access(std::string) = 0; // cheking access for cell's
     //virtual std::map<std::string, int> threat(std::string) = 0; // threat with cell ID and score
     //virtual void attack(std::string) = 0;
-    virtual void movePiece(std::string) = 0;
+    //virtual void movePiece(std::string) = 0;
 };
 
 #endif /* _CHESSMAN_H */
--- include/rook.h
+++ include/rook.h
@@ -7,10 +7,11 @@
 class rook : public ChessMan
 {
 public:
-    virtual void movePiece(MOVE);
+    rook(COLOR);
+    //virtual void movePiece(MOVE);
     virtual bool access(std::string); // cheking access for cell's
-    virtual std::map<std::string, int> threat(std::string); // threat with cell ID and score
-    virtual void attack(std::string);
+    //virtual std::map<std::string, int> threat(std::string); // threat with cell ID and score
+    //virtual void attack(std::string);
 };
 
 #endif /* _ROOK_H */
