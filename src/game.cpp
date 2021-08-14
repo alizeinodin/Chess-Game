d935d40d3984aaa845baac0dfb22bbdd90e496db
erfan-hamidi <hamidierfan716@gmail.com>
2021-08-14 22:11:36 +0430

    debug undo enpassant

--- src/game.cpp
+++ src/game.cpp
@@ -542,7 +542,7 @@ QString Game::undo(bool isreview)
             throw e;
         }
     }
-    temp = move.substr(2, 5);
+    //temp = move.substr(2, 5);
     cout << "move undo: " << temp << endl;
     string makeResult = temp[0] + temp.substr(3, 4) + temp.substr(1, 2);
     cout << "make result: " << makeResult << endl;
